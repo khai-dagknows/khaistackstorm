e1ce39459332db2b2fe3283d33be49f508f32180
Khai Doan <khai@dagknows.com>
2023-02-02 12:38:13 -0800

    adding files

--- actions/parse_memory_usage.py
+++ actions/parse_memory_usage.py
@@ -3,6 +3,6 @@ import sys
 from st2common.runners.base_action import Action
 
 class ParseMemoryUsage(Action):
-    def run(self, str):
-        print("ParseMemoryUsage invoked")
-				return (True, 2286456)
+  def run(self, str):
+    print("ParseMemoryUsage invoked")
+    return (True, 2286456)
