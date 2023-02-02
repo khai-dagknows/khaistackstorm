f5d1f28c700fed804f93bb5f4724abd0124f20d0
Khai Doan <khai@dagknows.com>
2023-02-02 12:06:05 -0800

    adding files

---
+++ actions/parse_memory_usage.py
@@ -0,0 +1,8 @@
+import sys
+
+from st2common.runners.base_action import Action
+
+class ParseMemoryUsage(Action):
+    def run(self, str):
+        print("ParseMemoryUsage invoked")
+				return (True, 2286456)
---
+++ actions/top_memory_consumers.py
@@ -0,0 +1,8 @@
+import sys
+
+from st2common.runners.base_action import Action
+
+class TopMemoryConsumers(Action):
+    def run(self, str):
+        print("TopMemoryConsumers invoked with: " + str)
+				return (True, str)
