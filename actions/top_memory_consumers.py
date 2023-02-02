637e1c1ed6b495b0b916f7fb2a95391c2bb83fd3
Khai Doan <khai@dagknows.com>
2023-02-02 12:41:07 -0800

    adding files

--- actions/top_memory_consumers.py
+++ actions/top_memory_consumers.py
@@ -3,6 +3,6 @@ import sys
 from st2common.runners.base_action import Action
 
 class TopMemoryConsumers(Action):
-    def run(self, str):
-        print("TopMemoryConsumers invoked with: " + str)
-				return (True, str)
+  def run(self, str):
+    print("TopMemoryConsumers invoked with: " + str)
+    return (True, str)
